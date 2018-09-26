0c27938f3513ba22aa8c6dbbc7b3e3f8ba2d6679
rami9284 <judramirez@csumb.edu>
2018-09-25 21:40:32 -0700

    Adding Files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+// component decorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
+ // logic of the component
 export class AppComponent {
   title = 'angularP';
 }
